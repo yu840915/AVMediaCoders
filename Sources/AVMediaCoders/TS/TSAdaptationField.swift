e2c34a3feb7bc4d35d8f14ae6983acc32a9411a9
yu840915 <yu840915@gmail.com>
2025-03-31 23:25:07 +0800

    Main part of adaptation field

---
+++ Sources/AVMediaCoders/TS/TSAdaptationField.swift
@@ -0,0 +1,154 @@
+struct TSAdaptationField: Equatable {
+  let length: UInt8
+  let pcr: TSClockReference?
+  let opcr: TSClockReference?
+  var randomAccessIndicator: Bool {
+    flags.randomAccessIndicator
+  }
+  let spliceCountdown: UInt8?
+  let transportPrivateData: [UInt8]?
+  let flags: Flags
+
+  let bytes: [UInt8]
+
+  init(
+    pcr: TSClockReference? = nil,
+    opcr: TSClockReference? = nil,
+    randomAccessIndicator: Bool = false
+  ) {
+    self.pcr = pcr
+    self.opcr = opcr
+    flags = Flags(
+      discontinuityIndicator: false,
+      randomAccessIndicator: randomAccessIndicator,
+      elementaryStreamPriorityIndicator: false,
+      pcrFlag: pcr != nil,
+      opcrFlag: opcr != nil,
+      splicingPointFlag: false,
+      transportPrivateDataFlag: false,
+      adaptationFieldExtensionFlag: false
+    )
+    self.spliceCountdown = nil
+    self.transportPrivateData = nil
+    let bytes: [UInt8] = flags.bytes + (pcr?.bytes ?? []) + (opcr?.bytes ?? [])
+    self.length = UInt8(bytes.count)
+    self.bytes = [length] + bytes
+  }
+
+  init(bytes: [UInt8]) throws {
+    guard bytes.count >= 2 else {
+      throw AVMediaCodersError.bufferTooShort
+    }
+    flags = try Flags(bytes: Array(bytes[1...1]))
+    var remainingBytes = Array(bytes[2...])
+    length = bytes[0]
+    guard bytes.count > length else {
+      throw AVMediaCodersError.bufferTooShort
+    }
+    if flags.pcrFlag {
+      pcr = try TSClockReference(bytes: Array(remainingBytes[0...5]))
+      remainingBytes = Array(remainingBytes[6...])
+    } else {
+      pcr = nil
+    }
+    if flags.opcrFlag {
+      opcr = try TSClockReference(bytes: Array(remainingBytes[0...5]))
+      remainingBytes = Array(remainingBytes[6...])
+    } else {
+      opcr = nil
+    }
+    if flags.splicingPointFlag {
+      spliceCountdown = remainingBytes[0]
+      remainingBytes = Array(remainingBytes[1...])
+    } else {
+      spliceCountdown = nil
+    }
+    if flags.transportPrivateDataFlag {
+      let length = remainingBytes[0]    
+      transportPrivateData = Array(remainingBytes[1...Int(length)])
+      remainingBytes = Array(remainingBytes[Int(length + 1)...])
+    } else {
+      transportPrivateData = nil
+    }    
+    self.bytes = Array(bytes[0...Int(length)])
+  }
+}
+
+extension TSAdaptationField {
+  struct Flags: Equatable {
+    let discontinuityIndicator: Bool
+    let randomAccessIndicator: Bool
+    let elementaryStreamPriorityIndicator: Bool
+    let pcrFlag: Bool
+    let opcrFlag: Bool
+    let splicingPointFlag: Bool
+    let transportPrivateDataFlag: Bool
+    let adaptationFieldExtensionFlag: Bool
+
+    var bytes: [UInt8] {
+      var value: UInt8 = 0
+      value |= (discontinuityIndicator ? 0b10000000 : 0)
+      value |= (randomAccessIndicator ? 0b01000000 : 0)
+      value |= (elementaryStreamPriorityIndicator ? 0b00100000 : 0)
+      value |= (pcrFlag ? 0b00010000 : 0)
+      value |= (opcrFlag ? 0b00001000 : 0)
+      value |= (splicingPointFlag ? 0b00000100 : 0)
+      value |= (transportPrivateDataFlag ? 0b00000010 : 0)
+      value |= (adaptationFieldExtensionFlag ? 0b00000001 : 0)
+      return [value]
+    }
+
+    init(
+      discontinuityIndicator: Bool = false,
+      randomAccessIndicator: Bool = false,
+      elementaryStreamPriorityIndicator: Bool = false,
+      pcrFlag: Bool = false,
+      opcrFlag: Bool = false,
+      splicingPointFlag: Bool = false,
+      transportPrivateDataFlag: Bool = false,
+      adaptationFieldExtensionFlag: Bool = false
+    ) {
+      self.discontinuityIndicator = discontinuityIndicator
+      self.randomAccessIndicator = randomAccessIndicator
+      self.elementaryStreamPriorityIndicator = elementaryStreamPriorityIndicator
+      self.pcrFlag = pcrFlag
+      self.opcrFlag = opcrFlag
+      self.splicingPointFlag = splicingPointFlag
+      self.transportPrivateDataFlag = transportPrivateDataFlag
+      self.adaptationFieldExtensionFlag = adaptationFieldExtensionFlag
+    }
+
+    init(bytes: [UInt8]) throws {
+      let byte = bytes[0]
+      self.discontinuityIndicator = (byte & 0b10000000) != 0
+      self.randomAccessIndicator = (byte & 0b01000000) != 0
+      self.elementaryStreamPriorityIndicator = (byte & 0b00100000) != 0
+      self.pcrFlag = (byte & 0b00010000) != 0
+      self.opcrFlag = (byte & 0b00001000) != 0
+      self.splicingPointFlag = (byte & 0b00000100) != 0
+      self.transportPrivateDataFlag = (byte & 0b00000010) != 0
+      self.adaptationFieldExtensionFlag = (byte & 0b00000001) != 0
+    }
+  }
+}
+
+struct AdaptationFieldExtension: Equatable {
+  let length: UInt8
+  let legalTimeWindowFlag: Bool
+  let piecewiseRateFlag: Bool
+  let seamlessSpliceFlag: Bool
+  let legalTimeWindowOffset: UInt16
+  let piecewiseRate: UInt32
+  let spliceType: UInt8
+  let dtsNextAccessUnit: UInt64
+}
+
+extension AdaptationFieldExtension {
+  struct ByteRepresentation {
+    let length: UInt8
+    let legalTimeWindowFlag: Bool
+    let piecewiseRateFlag: Bool
+    let seamlessSpliceFlag: Bool
+
+  }
+}
