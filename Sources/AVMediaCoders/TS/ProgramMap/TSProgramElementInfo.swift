97788f7714aa598ff96737a9bd4ba2bc129c9c0c
yu840915 <yu840915@gmail.com>
2025-04-22 23:03:59 +0800

    program element info

---
+++ Sources/AVMediaCoders/TS/ProgramMap/TSProgramElementInfo.swift
@@ -0,0 +1,125 @@
+struct TSProgramElementInfo: Equatable {
+  let streamType: TSStreamType
+  let elementaryPID: TSPID
+  let byteRepresentation: ByteRepresentation
+  var ESInfo: [UInt8] {
+    byteRepresentation.ESInfo
+  }
+  var bytes: [UInt8] {
+    byteRepresentation.bytes
+  }
+
+  init(
+    streamType: TSStreamType,
+    elementaryPID: TSPID,
+    ESInfo: [UInt8]
+  ) {
+    self.streamType = streamType
+    self.elementaryPID = elementaryPID
+    byteRepresentation = ByteRepresentation(
+      streamType: streamType.value,
+      elementaryPID: elementaryPID.value,
+      ESInfo: ESInfo
+    )
+  }
+
+  init(bytes: [UInt8]) throws {
+    byteRepresentation = try ByteRepresentation(bytes: bytes)
+    streamType = TSStreamType(rawValue: byteRepresentation.streamType)
+    elementaryPID = try TSPID(rawValue: byteRepresentation.elementaryPID)
+  }
+}
+
+extension TSProgramElementInfo {
+  struct ByteRepresentation: Equatable {
+    let streamType: UInt8
+    let elementaryPID: UInt16
+    let ESInfoLength: UInt16
+    let ESInfo: [UInt8]
+
+    init(
+      streamType: UInt8,
+      elementaryPID: UInt16,
+      ESInfo: [UInt8]
+    ) {
+      self.streamType = streamType
+      self.elementaryPID = elementaryPID
+      self.ESInfoLength = UInt16(ESInfo.count)
+      self.ESInfo = ESInfo
+    }
+
+    init(bytes: [UInt8]) throws {
+      guard bytes.count >= 5 else {
+        throw AVMediaCodersError.bufferTooShort
+      }
+      streamType = bytes[0]
+      elementaryPID = UInt16(bytes[1] & 0x1F) << 8 | UInt16(bytes[2])
+      ESInfoLength = UInt16(bytes[3] & 0x0F) << 8 | UInt16(bytes[4])
+      let infoBytes = bytes.dropFirst(5)
+      let infoLength = Int(ESInfoLength)
+      guard infoLength <= infoBytes.count else {
+        throw AVMediaCodersError.bufferTooShort
+      }
+      ESInfo = Array(infoBytes.prefix(infoLength))
+    }
+
+    var bytes: [UInt8] {
+      let pidBytes = elementaryPID.bigEndianBytes
+      let part2 = pidBytes[0] & 0x1F
+      let part3 = pidBytes[1]
+      let lenBytes = ESInfoLength.bigEndianBytes
+      let part4 = lenBytes[0] & 0x0F
+      let part5 = lenBytes[1]
+      return [streamType, part2, part3, part4, part5] + ESInfo
+    }
+  }
+}
+
+enum TSStreamType: Equatable {
+  case videoMPEG1
+  case videoMPEG2
+  case audioMPEG1
+  case audioMPEG2HalvedSampleRate
+  case subtitle
+  case mhegFeatures
+  case audioADTSAAC
+  case videoAVC
+  case videoHEVC
+  case audioATSCDolbyDigital
+  case IPMP
+  case unsupported(value: UInt8)
+
+  init(rawValue: UInt8) {
+    switch rawValue {
+    case 0x01: self = .videoMPEG1
+    case 0x02: self = .videoMPEG2
+    case 0x03: self = .audioMPEG1
+    case 0x04: self = .audioMPEG2HalvedSampleRate
+    case 0x05: self = .subtitle
+    case 0x06: self = .mhegFeatures
+    case 0x0F: self = .audioADTSAAC
+    case 0x1B: self = .videoAVC
+    case 0x24: self = .videoHEVC
+    case 0x87: self = .audioATSCDolbyDigital
+    case 0x7F: self = .IPMP
+    default: self = .unsupported(value: rawValue)
+    }
+  }
+
+  var value: UInt8 {
+    switch self {
+    case .videoMPEG1: 0x01
+    case .videoMPEG2: 0x02
+    case .audioMPEG1: 0x03
+    case .audioMPEG2HalvedSampleRate: 0x04
+    case .subtitle: 0x05
+    case .mhegFeatures: 0x06
+    case .audioADTSAAC: 0x0F
+    case .videoAVC: 0x1B
+    case .videoHEVC: 0x24
+    case .audioATSCDolbyDigital: 0x87
+    case .IPMP: 0x7F
+    case .unsupported(let value): value
+    }
+  }
+}
---
+++ Tests/AVMediaCodersTests/TSProgramElementInfoTests.swift
@@ -0,0 +1,75 @@
+import Testing
+
+@testable import AVMediaCoders
+
+struct TSProgramElementInfoTests {
+  @Test
+  func initialization() async throws {
+    let sut = TSProgramElementInfo(
+      streamType: .videoHEVC,
+      elementaryPID: .dataStream(streamID: 1),
+      ESInfo: [0xFF, 0x42]
+    )
+
+    #expect(sut.streamType == .videoHEVC)
+    #expect(sut.elementaryPID == .dataStream(streamID: 1))
+    #expect(sut.ESInfo == [0xFF, 0x42])
+    #expect(sut.ESInfo.count == 2)
+  }
+
+  @Test
+  func encode() async throws {
+    let sut = TSProgramElementInfo(
+      streamType: .videoHEVC,
+      elementaryPID: .dataStream(streamID: 1),
+      ESInfo: [0xFF, 0x42]
+    )
+
+    let bytes = sut.bytes
+
+    #expect(
+      bytes == [
+        0x24,  // hevc
+        0x00, 0x21,  // elementaryPID
+        0x00, 0x02,  // ESInfoLength
+        0xFF, 0x42,  // ESInfo
+      ]
+    )
+  }
+
+  @Test
+  func encodeAndDecode() async throws {
+    let src = TSProgramElementInfo(
+      streamType: .videoHEVC,
+      elementaryPID: .dataStream(streamID: 1),
+      ESInfo: [0xFF, 0x42]
+    )
+    let bytes = src.bytes
+
+    let sut = try TSProgramElementInfo(bytes: bytes)
+
+    #expect(sut == src)
+  }
+
+  @Test(arguments: [
+    [0x24, 0x00, 0x21, 0x00],  //truncated meta
+    [0x24, 0x00, 0x21, 0x00, 0x02, 0xFF],  // truncated info
+  ])
+  func detectBufferTooShort(_ bytes: [UInt8]) async throws {
+    #expect(throws: AVMediaCodersError.bufferTooShort) {
+      _ = try TSProgramElementInfo(bytes: bytes)
+    }
+  }
+
+  @Test
+  func keepJustEnoughtBufferLength() async throws {
+    let bytes: [UInt8] = [
+      0x24, 0x00, 0x21, 0x00, 0x02, 0xFF, 0x42,
+      0x24,  //Start of next element
+    ]
+
+    let sut = try TSProgramElementInfo(bytes: bytes)
+
+    #expect(sut.bytes == [0x24, 0x00, 0x21, 0x00, 0x02, 0xFF, 0x42])
+  }
+}
