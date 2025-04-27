38b86cfb80da98ebc68c683a7111ff14804667bd
yu840915 <yu840915@gmail.com>
2025-04-27 10:40:13 +0800

    PMT

---
+++ Sources/AVMediaCoders/TS/ProgramMap/TSProgramMapTable.swift
@@ -0,0 +1,61 @@
+struct TSProgramMapTable: Equatable {
+  let programNumber: UInt16
+  private(set) var versionNumber: UInt8
+  private(set) var PCRPID: TSPID
+  private(set) var programInfo: [UInt8]
+  private(set) var programElementInfos: [TSProgramElementInfo]
+
+  init(
+    programNumber: UInt16,
+    versionNumber: UInt8 = 0,
+    PCRPID: TSPID = .dataStream(streamID: 0),
+    programInfo: [UInt8] = [],
+    programElementInfos: [TSProgramElementInfo] = []
+  ) {
+    self.programNumber = programNumber
+    self.versionNumber = versionNumber
+    self.PCRPID = PCRPID
+    self.programInfo = programInfo
+    self.programElementInfos = programElementInfos
+  }
+
+  init(section: TSProgramMapSection) {
+    self.programNumber = section.programNumber
+    self.versionNumber = section.versionNumber
+    self.PCRPID = section.PCRPID
+    self.programInfo = section.programInfo
+    self.programElementInfos = section.programElementInfos
+  }
+
+  mutating func update(
+    _ updater: (inout TSPID, inout [UInt8], inout [TSProgramElementInfo]) -> Void
+  ) {
+    let oldPCRPID = PCRPID
+    let oldProgramInfo = programInfo
+    let oldProgramElementInfos = programElementInfos
+    updater(&PCRPID, &programInfo, &programElementInfos)
+    if oldPCRPID != PCRPID || oldProgramInfo != programInfo
+      || oldProgramElementInfos != programElementInfos
+    {
+      incrementVersion()
+    }
+  }
+
+  private mutating func incrementVersion() {
+    if versionNumber < 0x1F {
+      versionNumber += 1
+    } else {
+      versionNumber = 0
+    }
+  }
+
+  func convertToSection() throws -> TSProgramMapSection {
+    try TSProgramMapSection(
+      programNumber: programNumber,
+      versionNumber: versionNumber,
+      PCRPID: PCRPID,
+      programInfo: programInfo,
+      programElementInfos: programElementInfos
+    )
+  }
+}
---
+++ Tests/AVMediaCodersTests/TSProgramMapTableTests.swift
@@ -0,0 +1,205 @@
+import Testing
+
+@testable import AVMediaCoders
+
+struct TSProgramMapTableTests {
+  @Test
+  func initValue() async throws {
+    let sut = TSProgramMapTable(programNumber: 1)
+
+    #expect(sut.programNumber == 1)
+    #expect(sut.versionNumber == 0)
+    #expect(sut.PCRPID == .dataStream(streamID: 0))
+    #expect(sut.programInfo == [])
+    #expect(sut.programElementInfos == [])
+  }
+
+  @Test
+  func updateProgramInfoAndIncrementVersion() async throws {
+    var sut = TSProgramMapTable(programNumber: 1)
+
+    sut.update { _, programInfo, _ in
+      programInfo.append(0x01)
+    }
+
+    #expect(sut.versionNumber == 1)
+    #expect(sut.programInfo == [0x01])
+  }
+
+  @Test
+  func updatePCRPIDAndIncrementVersion() async throws {
+    var sut = TSProgramMapTable(programNumber: 1)
+
+    sut.update { PCRPID, _, _ in
+      PCRPID = .dataStream(streamID: 0x01)
+    }
+
+    #expect(sut.versionNumber == 1)
+    #expect(sut.PCRPID == .dataStream(streamID: 0x01))
+  }
+
+  @Test
+  func updateProgramElementInfosAndIncrementVersion() async throws {
+    var sut = TSProgramMapTable(programNumber: 1)
+
+    sut.update { _, _, programElementInfos in
+      programElementInfos.append(
+        TSProgramElementInfo(
+          streamType: .videoHEVC,
+          elementaryPID: .dataStream(streamID: 0x01),
+          ESInfo: []
+        )
+      )
+    }
+
+    #expect(sut.versionNumber == 1)
+    #expect(
+      sut.programElementInfos == [
+        TSProgramElementInfo(
+          streamType: .videoHEVC,
+          elementaryPID: .dataStream(streamID: 0x01),
+          ESInfo: []
+        )
+      ]
+    )
+  }
+
+  @Test
+  func updateAndIncrementVersion() async throws {
+    var sut = TSProgramMapTable(programNumber: 1)
+
+    sut.update { PCRPID, programInfo, programElementInfos in
+      PCRPID = .dataStream(streamID: 0x01)
+      programInfo.append(0x01)
+      programElementInfos.append(
+        TSProgramElementInfo(
+          streamType: .videoHEVC,
+          elementaryPID: .dataStream(streamID: 0x01),
+          ESInfo: []
+        )
+      )
+    }
+
+    #expect(
+      sut
+        == TSProgramMapTable(
+          programNumber: 1,
+          versionNumber: 1,
+          PCRPID: .dataStream(streamID: 0x01),
+          programInfo: [0x01],
+          programElementInfos: [
+            TSProgramElementInfo(
+              streamType: .videoHEVC,
+              elementaryPID: .dataStream(streamID: 0x01),
+              ESInfo: []
+            )
+          ]
+        )
+    )
+  }
+
+  @Test
+  func doNotIncrementIfNotChanged() async throws {
+    var sut = TSProgramMapTable(programNumber: 1)
+
+    sut.update { _, programInfo, _ in
+      programInfo.removeAll()
+    }
+
+    #expect(sut.versionNumber == 0)
+  }
+
+  @Test
+  func doNotOverflowVersion() async throws {
+    var sut = TSProgramMapTable(
+      programNumber: 1,
+      versionNumber: 0x1F,
+      programInfo: [0x42]
+    )
+
+    sut.update { _, programInfo, _ in
+      programInfo.removeAll()
+    }
+
+    #expect(
+      sut
+        == TSProgramMapTable(
+          programNumber: 1,
+          versionNumber: 0x00,
+          programInfo: []
+        )
+    )
+  }
+
+  @Test
+  func convertToSection() async throws {
+    let sut = TSProgramMapTable(
+      programNumber: 1,
+      versionNumber: 2,
+      PCRPID: .dataStream(streamID: 0x01),
+      programInfo: [0x01],
+      programElementInfos: [
+        TSProgramElementInfo(
+          streamType: .videoHEVC,
+          elementaryPID: .dataStream(streamID: 0x01),
+          ESInfo: []
+        )
+      ]
+    )
+
+    let section = try sut.convertToSection()
+
+    #expect(
+      try section
+        == TSProgramMapSection(
+          programNumber: 1,
+          versionNumber: 2,
+          PCRPID: .dataStream(streamID: 0x01),
+          programInfo: [0x01],
+          programElementInfos: [
+            TSProgramElementInfo(
+              streamType: .videoHEVC,
+              elementaryPID: .dataStream(streamID: 0x01),
+              ESInfo: []
+            )
+          ]
+        )
+    )
+  }
+
+  @Test
+  func initWithSection() async throws {
+    let sut = TSProgramMapTable(
+      section: try TSProgramMapSection(
+        programNumber: 1,
+        versionNumber: 2,
+        PCRPID: .dataStream(streamID: 0x01),
+        programInfo: [0x01],
+        programElementInfos: [
+          TSProgramElementInfo(
+            streamType: .videoHEVC,
+            elementaryPID: .dataStream(streamID: 0x01),
+            ESInfo: []
+          )
+        ]
+      )
+    )
+
+    #expect(
+      sut
+        == TSProgramMapTable(
+          programNumber: 1,
+          versionNumber: 2,
+          PCRPID: .dataStream(streamID: 0x01),
+          programInfo: [0x01],
+          programElementInfos: [
+            TSProgramElementInfo(
+              streamType: .videoHEVC,
+              elementaryPID: .dataStream(streamID: 0x01),
+              ESInfo: []
+            )
+          ]
+        )
+    )
+  }
+}
